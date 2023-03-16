0342ba2f837afd421453e7f3633e419ab75fb9f5
sassnoe <105166994+sassnoe@users.noreply.github.com>
2023-03-16 11:15:47 +0100

    Inches to cm & cm to Inches

--- conversions.js
+++ conversions.js
@@ -7,3 +7,13 @@ function lToG(liters) {
   const gallons = liters / 3.78541;
   return gallons;
 }
+
+/* ======== Inches to CM & CM to Inches ======== */
+
+function inchToCm(inches) {
+  return inches / 0.393701;
+}
+
+function cmToInch(centimeters) {
+  return centimeters / 2.54;
+}
